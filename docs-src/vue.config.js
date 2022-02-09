ba00dee65b4ad8e4f597159718cbbbaf07edf63c
chenzhilong <chenzhilong@zhuozhuo.io>
2022-02-09 21:05:43 +0800

    add docs

---
+++ docs-src/vue.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+
+console.log(`\n${process.env.NODE_ENV} 环境\n`);
+module.exports = {
+  publicPath: './',
+
+  outputDir: path.resolve(__dirname, '../docs'),
+
+  productionSourceMap: false
+}
