b77283359b5176a24fe2de5f9d1d2b7019e8a4db
chenzhilong <chenzhilong@zhuozhuo.io>
2022-02-10 14:47:06 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vue-components
 vue通用组件、指令
+
+[示例](https://zlong1010.github.io/vue-components/)
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
-const path = require("path");
+const { resolve } = require('path');
 
 console.log(`\n${process.env.NODE_ENV} 环境\n`);
 module.exports = {
-  publicPath: "./",
-
-  outputDir: path.resolve(__dirname, "./docs"),
-
   productionSourceMap: false,
+  publicPath: './',
+  outputDir: resolve(__dirname, './docs'),
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@cmp': resolve('components'), // 组件目录
+        '@dire': resolve('directives'), // 指令目录
+      },
+    },
+  },
 };
